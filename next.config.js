36cc713933fa03ad10a1a0bebc246f0297635d2d
bio-design <bio-design@users.noreply.github.com>
2025-01-14 18:34:05 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/taxonomy-tree',
+  basePath: '/taxonomy-tree'
 }
 
 module.exports = nextConfig
